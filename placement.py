ba803326f22ea248cb6b8565f949f26881557d2e
Aditya Sudhakar <adityasudhakar@Adityas-Air.olin.edu>
2021-12-09 12:18:52 -0500

    initial commit

---
+++ placement.py
@@ -0,0 +1,42 @@
+######################################################################
+#
+# DISCRETE FINAL PROJECT 
+#
+# Date: December 2021
+# 
+# Contributors: Aditya Sudhakar, Lila Smith, Dasha Chadiyuk
+# 
+# Emails:
+# 
+# Remarks, if any:
+#
+#
+#
+#
+######################################################################
+
+
+
+def consume_circuit():
+    #TODO IMPLEMENT
+    pass
+
+def generate_netlist_dict():
+    #TODO IMPLEMENT
+    pass
+
+def generate_layout_grid():
+    #TODO IMPLEMENT
+    pass
+
+def calculate_minimum_crossings():
+    #TODO IMPLEMENT
+    pass
+
+def squish():
+    #TODO IMPLEMENT
+    pass
+
+def optimize_me():
+    #TODO IMPLEMENT
+    pass
